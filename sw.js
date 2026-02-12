fd0fa259a607d32b4dd9a9c85a9607b0b2e94e18
fatihctn4157-pixel <fatihctn4157@gmail.com>
2026-02-12 21:51:17 +0300

    Update sw.js
    3

--- sw.js
+++ sw.js
@@ -1,13 +1,22 @@
-self.addEventListener('install',e=>{
-e.waitUntil(
-caches.open('bordro-v2').then(cache=>{
-return cache.addAll(['index.html']);
-})
-);
+const CACHE_NAME = "bordro2026-v1";
+
+const urlsToCache = [
+  "index.html",
+  "manifest.json",
+  "icon-192.png",
+  "icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
 
-self.addEventListener('fetch',e=>{
-e.respondWith(
-caches.match(e.request).then(res=>res||fetch(e.request))
-);
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });
